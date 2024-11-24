c4fe4c2a3fc76a0ae90d20c7da11bfafb79aa5ab
Vineeth0502 <vineethketham@gmail.com>
2024-11-23 19:42:14 -0600

    changes updated

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const dbURI = "mongodb+srv://mdrohitreddy:mdrohitreddy@cluster0.bzga5vf.mongodb.net/teachers";
+const dbURI = "mongodb+srv://admin:<pollingsystem123>@polling.y4g05.mongodb.net/?retryWrites=true&w=majority&appName=Polling";
 
 const connectToDB = async () => {
     try {
